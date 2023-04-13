9ad1d7a0dd8b8f0f1ad47933eb7b9a8975202a3d
LegendisM <makenkap@gmail.com>
2023-04-13 15:11:33 +0330

    Complete Signin/Signup Action & Store Access Token

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,12 +1,17 @@
-import { IAuthResponseDto } from "@/common/interfaces/auth/auth.dto";
-import { useAxios } from "@/common/services/api.service";
-import Alerts, { AlertColors } from "@/components/common/alerts";
 import Layout from "@/components/layout";
-import { GetApiRoute } from "@/constants/api.config";
-import { Anchor, Button, Container, Paper, PasswordInput, TextInput, Title, Text, Space, Box } from "@mantine/core";
+import { Anchor, Button, Container, Paper, PasswordInput, TextInput, Title, Text, Box, Space, LoadingOverlay } from "@mantine/core";
 import { hasLength, useForm } from "@mantine/form";
+import { useAxios } from "@/common/helpers/api.helper";
+import { IAuthResponseDto } from "@/common/interfaces/auth/auth.dto";
+import { GetApiRoute } from "@/constants/api.config";
+import Alerts, { AlertColors } from "@/components/common/alerts";
+import { AxiosResponse } from "axios";
+import { useState } from "react";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
 
 export default function SignInPage() {
+    const router = useRouter();
     const form = useForm({
         initialValues: {
             username: '',
@@ -22,9 +27,22 @@ export default function SignInPage() {
         method: 'POST',
         data: form.values
     });
+    const [token, setToken] = useLocalStorage({ key: 'token' });
+    const [overlay, setOverlay] = useState(false);
+
+    const onSigninResponse = (response: AxiosResponse<IAuthResponseDto>) => {
+        if (response.data.state) {
+            setToken(response.data.token);
+            setOverlay(true);
+            setTimeout(() => {
+                router.push("/");
+            }, 1000)
+        }
+    }
 
     return (
         <Layout pageKey="signin" title="Sign In" description="User Auth SignIn">
+            <LoadingOverlay visible={overlay} />
             <Container size={'xs'} my={40}>
                 <Title
                     align="center"
@@ -40,7 +58,9 @@ export default function SignInPage() {
                 </Text>
 
                 <Paper withBorder shadow="md" p={30} pb={25} mt={30} radius="md">
-                    <Box component="form" onSubmit={form.onSubmit((values) => { signin(); })}>
+                    <Box component="form" onSubmit={form.onSubmit((values) => {
+                        signin().then(onSigninResponse);
+                    })}>
                         <TextInput
                             name="username"
                             label="Username"
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,12 +1,17 @@
 import Layout from "@/components/layout";
-import { Anchor, Button, Container, Paper, PasswordInput, TextInput, Title, Text, Box, Space } from "@mantine/core";
+import { Anchor, Button, Container, Paper, PasswordInput, TextInput, Title, Text, Box, Space, LoadingOverlay } from "@mantine/core";
 import { hasLength, useForm } from "@mantine/form";
-import { useAxios } from "@/common/services/api.service";
+import { useAxios } from "@/common/helpers/api.helper";
 import { IAuthResponseDto } from "@/common/interfaces/auth/auth.dto";
 import { GetApiRoute } from "@/constants/api.config";
 import Alerts, { AlertColors } from "@/components/common/alerts";
+import { AxiosResponse } from "axios";
+import { useState } from "react";
+import { useLocalStorage } from "@mantine/hooks";
+import { useRouter } from "next/router";
 
 export default function SignUpPage() {
+    const router = useRouter();
     const form = useForm({
         initialValues: {
             username: '',
@@ -22,9 +27,22 @@ export default function SignUpPage() {
         method: 'POST',
         data: form.values
     });
+    const [overlay, setOverlay] = useState(false);
+    const [token, setToken] = useLocalStorage({ key: 'token' });
+
+    const onSignupResponse = (response: AxiosResponse<IAuthResponseDto>) => {
+        if (response.data.state) {
+            setToken(response.data.token);
+            setOverlay(true);
+            setTimeout(() => {
+                router.push("/");
+            }, 1000);
+        }
+    }
 
     return (
         <Layout pageKey="signup" title="Sign Up" description="User Auth SignUp">
+            <LoadingOverlay visible={overlay} />
             <Container size={'xs'} my={40}>
                 <Title
                     align="center"
@@ -40,7 +58,9 @@ export default function SignUpPage() {
                 </Text>
 
                 <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                    <Box component="form" onSubmit={form.onSubmit((values) => { signup(); })}>
+                    <Box component="form" onSubmit={form.onSubmit((values) => {
+                        signup().then(onSignupResponse);
+                    })}>
                         <TextInput
                             name="username"
                             label="Username"
@@ -56,7 +76,7 @@ export default function SignUpPage() {
                             required
                             {...form.getInputProps('password')}
                         />
-                        <Button type="submit" fullWidth mt="xl">
+                        <Button type="submit" fullWidth mt="xl" loading={loading}>
                             Sign up
                         </Button>
                         <Space h={'md'} />
